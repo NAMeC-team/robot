2696340fc93e884e12eaba8bd67110b212e9f5ae
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2025-11-07 12:43:12 +0100

    IR test binary sending results on serial USB

--- src/sensor/ir.cpp
+++ src/sensor/ir.cpp
@@ -7,10 +7,16 @@ namespace ir {
     DigitalOut emit(IR_EMIT, 0.0);
     AnalogIn receive(IR_RECEIVE);
 
+    UnbufferedSerial serial_port(USBTX, USBRX); // probably 9600 baudrate, I kinda forgot
+
     void compute() {
         emit = 1;
         value = receive.read();
         emit = 0;
+        char buffer[32];
+        sprintf(buffer, "IR: %s\n", value ? "ON" : "off");
+        // sprintf(buffer, "IR: %f\n", value);  // use this variant to print the IR value perceived instead
+        serial_port.write(buffer, strlen(buffer));
     }
 
     bool present() { return value < IR_THRESHOLD ? true : false; }
